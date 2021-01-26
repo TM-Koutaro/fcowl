7b2c75919bb60ef1fa632eff3681261fe70eeba0
Koutaro MAEDA <maeda@booklista.co.jp>
2021-01-26 13:04:10 +0900

    --add 「@nuxtjs/amp」 を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -96,11 +96,9 @@ export default {
    ** Nuxt.js dev-modules
    */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
-    // Simple usage
+    '@nuxtjs/amp',
     '@nuxtjs/netlify-files',
     '@nuxtjs/gtm',
   ],
